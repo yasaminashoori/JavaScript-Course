62babf2f7ee546e976f89ed96da69893b88b9641
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-11 16:30:43 +0430

    Add part 5: data types

--- DataTypes.js
+++ DataTypes.js
@@ -1,2 +1,32 @@
-var firstNum = 34
-let secondNum = 234.65
+// Datatypes
+
+const age = 12;  // NUmber: int, float
+
+const userName = "Yas"; //string: chars, text
+
+const isActive = true; //Boolean: true, false
+
+let favorites //undefined
+
+const likedPosts = null; //Null: empty value or nothing
+
+// Symbol: Unique value and unchangable
+// BigInt: numeric value and too large
+
+let userAge = 28;
+
+let userName2 = "Yasamin";
+
+const isAlive = true;
+
+let users;
+
+const archivedPosts = null;
+
+console.log(userAge, userName);
+
+console.log(typeof userAge);
+
+console.log(users);
+
+console.log(typeof archivedPosts);
