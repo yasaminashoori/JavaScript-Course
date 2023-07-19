e15d3737cf328417b45b747f1bcfc3530f0c3736
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-19 22:42:03 +0430

    Add while/ do-while

---
+++ Season3-Control-Flow/22.While-do-while.js
@@ -0,0 +1,11 @@
+let i = 1;
+while (i <= 10) {
+    if (i % 2 === 0) console.log("the even number is", i);
+    i++;
+}
+
+let j = 1;
+do {
+    if (j % 2 === 0) console.log("the even number is", i);
+    j++;
+} while (i <= 10);
