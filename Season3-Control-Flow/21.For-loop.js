b3acd90993dec560f98e59e45a1d2987b97d18e9
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-19 22:42:17 +0430

    Add for loop

---
+++ Season3-Control-Flow/21.For-loop.js
@@ -0,0 +1,10 @@
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) console.log("the even number is", i);
+}
+
+for (let i = 10; i >= 1; i--) {
+    if (i % 2 === 0) console.log("the even number is", i);
+}
+
+
+console.log(i); //Undefined: outside the loop 
