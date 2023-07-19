1c6c32199a5849c988d9ce2a5017a4e29eb89de9
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-19 22:39:30 +0430

    Add control flow

---
+++ Season3-Control-Flow/19.If-else.js
@@ -0,0 +1,11 @@
+const paymentStatus = "SUCCESS";
+const isVerifiedPayment = true;
+
+if (isVerifiedPayment && paymentStatus === "SUCCESS") {
+
+    console.log("This is verified payment.");
+} else if(paymentStatus === "PENDING"){
+    console.log("This is pending payment. ");
+}else{
+    console.log("This is rejected payment.");
+}
