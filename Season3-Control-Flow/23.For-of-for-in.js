01adc234e99db75507aba5d35f408a9ce46a5d49
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-22 16:26:19 +0430

    Add for in

---
+++ Season3-Control-Flow/23.For-of-for-in.js
@@ -0,0 +1,20 @@
+const user ={
+    name: 'yas',
+    email: 'yas@gmail.com',
+    name: '09213456784',
+};
+
+
+for (const key in user) {
+    console.log(user[key]);
+}
+
+const roles = ["admin", "student", "teacher"];
+
+for (const role in roles) {
+    console.log(role, roles[index]);
+}
+
+for (const item of roles) {
+    console.log(item);
+}
