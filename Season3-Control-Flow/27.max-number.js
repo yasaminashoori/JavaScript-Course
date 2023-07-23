527b2a26fc731807cd97cf678b4d9795879f7388
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-23 23:35:40 +0430

    Add max-number

---
+++ Season3-Control-Flow/27.max-number.js
@@ -0,0 +1,7 @@
+function maxNum(x, y) {
+    if (x > y) return "The max number is: " + x;
+    return "The max number is: " + y;
+}
+
+const result = maxNum(10, 32);
+console.log("the max number is :", result);
