cc2d496bf86cc298979524e80a75eb45d56c54be
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-23 22:36:23 +0430

    Add function

---
+++ Season3-Control-Flow/25.function.js
@@ -0,0 +1,13 @@
+// declare
+function sayHello(name) {
+    console.log("hi " + name + "from javascript course");
+}
+// call or invoke
+sayHello("yas");
+
+function Calculate_Area (radius) {
+    const area = 3.14 * radius ** 2;
+    console.log("the area is: ", area);
+}
+Calculate_Area(2);
+
