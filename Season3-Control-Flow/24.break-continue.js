99d6e6dfcdfc52b48fd8e00949ac7d134876e9df
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-23 22:36:08 +0430

    Add break continue

---
+++ Season3-Control-Flow/24.break-continue.js
@@ -0,0 +1,16 @@
+let i = 1;
+while (i <= 10) {
+    if (i >= 5) break;
+    if (i % 2 === 0) console.log("the even number is", i);
+    i++;
+}
+
+i = 1;
+while (i <= 10) {
+    if (i % 2 === 0) {
+        i++;
+        continue;
+    }
+    console.log(i);
+    i++;
+}
