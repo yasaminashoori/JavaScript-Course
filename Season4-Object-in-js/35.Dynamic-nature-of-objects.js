2558852a08007398ee976f1d496063cba51332ce
yasaminashoori <yasaminashoori18@gmail.com>
2023-07-25 16:45:53 +0430

    Add dynamic objects

---
+++ Season4-Object-in-js/35.Dynamic-nature-of-objects.js
@@ -0,0 +1,19 @@
+function createCourse(title, price, discount) {
+    this.title = title,
+    this.price = price,
+    this.discount =discount;
+    this.calcOfPrice = function () {
+        return price * (1 - discount / 100);
+    };
+}
+
+const course1 = new createCourse("next", 300, 34);
+course1.students = 120;
+delete course1.title;
+console.log(course1);
+console.log(course1.hasOwnProperty("price"));
+
+//get keys or values
+console.log(Object.keys(course1));
+console.log(Object.values(course1));
+console.log(Object.entries(course1));
