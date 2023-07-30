ebd41c3ecf43a8617a2ba8b611480a3b67b8b238
yasaminashoori <yasaminashoori@gmail.com>
2023-07-30 12:38:02 +0330

    Add number and math

---
+++ Season9-Number-and-Math/77.Numberandmath.js
@@ -0,0 +1,27 @@
+console.log(Number("34"));  
+console.log(+"34");  
+console.log(Number.parseInt("34.5rem", 10)); 
+console.log(Number.parseInt("34.5rem", 4)); 
+console.log(Number.parseFloat("34.5rem"));  
+console.log(Number.parseFloat("dd34.5rem")); //NAN 
+console.log(Number.parseInt("dd34.5rem")); //NAN 
+
+// rounding int
+
+console.log(Math.floor(23.56));
+console.log(Math.floor(23.9));
+console.log(Math.floor(-23.9));
+
+console.log(Math.ceil(23.2));
+console.log(Math.ceil(23.9));
+
+console.log(Math.round(23.9));
+console.log(Math.round(23.2));
+
+console.log(Math.trunc(23.9));
+console.log(Math.trunc(23.2));
+console.log(Math.trunc(-23.9));
+
+console.log(Math.floor(Math.random() * 8 + 1));
+
+ 
