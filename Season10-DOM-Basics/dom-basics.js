685c57aeb7d1945d536bcc2f2c1c7d52243594e6
yasaminashoori <yasaminashoori@gmail.com>
2023-08-13 15:39:12 +0330

    Add DOM

--- Season10-DOM-Basics/dom-basics.js
+++
@@ -1,9 +0,0 @@
-const course = document.querySelector(".course-title");
-course = document.querySelector(".course .course-title");
-console.log(course);
-console.log(course.tagName);
-console.log(course.classList);
-console.log(course.textContent);
-const courses = document.querySelectorAll(".course-title");
-console.log(courses);
-
