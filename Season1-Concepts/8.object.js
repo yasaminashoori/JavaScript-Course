fa6f43891cb9c1e3dbc97f925b92bd534fb4e816
yasaminashoori <yasaminashoori18@gmail.com>
2023-08-14 09:10:16 +0330

    Edit array content

---
+++ Season1-Concepts/8.object.js
@@ -0,0 +1,28 @@
+const user = {
+    name: "Yas",
+    age: 4,
+    address: "Iran",
+    isActive: true
+};
+
+user.name = "Me";
+delete user.isActive;
+
+console.log(typeof user);
+
+console.log(user);
+
+// dot notation
+console.log(user.name);
+
+// bracket notation
+console.log(user["name"]);
+console.log(user["age"]);
+console.log(user["address"]);
+
+const selectedProperty = "age";
+console.log(user[selectedProperty]);
+
+//not in the user object and it's undefined
+console.log(user[selectedProperty]);
+console.log(user.selectedProperty);
