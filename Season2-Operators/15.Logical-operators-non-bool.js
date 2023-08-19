86960c0bd8486d70e05d08c825abd46c03d0ed0f
yasaminashoori <yasaminashoori18@gmail.com>
2023-08-19 15:25:43 +0330

    Add season2

--- Season2-Operators/15.Logical-operators-non-bool.js
+++ Season2-Operators/15.Logical-operators-non-bool.js
@@ -1,6 +1,15 @@
+console.log(true || 0);
+console.log(true || "yas");
+console.log(0 || "yas");
+console.log(null || "yas");
+console.log(" " || 'yas');
+console.log(" " || 'yas');
+console.log(" " || 'yas');
+
 const selectedColor = "blue";
 const defaultColor = "black";
 const userColor = selectedColor || defaultColor;
+
 userColor = selectedColor ? selectedColor : defaultColor; //Ternary
 console.log(userColor);
 console.log(Boolean(userColor)); //Converting to boolean
