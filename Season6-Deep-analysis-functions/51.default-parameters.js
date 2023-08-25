66f023eb256e25e71b768370bc8ecd11efa181f9
yasaminashoori <yasaminashoori18@gmail.com>
2023-08-25 22:23:05 +0430

    Add changes on season6

---
+++ Season6-Deep-analysis-functions/51.default-parameters.js
@@ -0,0 +1,8 @@
+function calceOfPrice(numbers, discount = 0.3 ) {
+    let total = 0;
+    for (const items of numbers) total += item;
+    console.log(total * (1 -  discount));
+    return total * (1 -  discount);
+}
+
+calceOfPrice([1, 2, 3, 4], 0.3)
