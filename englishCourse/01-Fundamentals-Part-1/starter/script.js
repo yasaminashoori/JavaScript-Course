b8e9ba994d22325c5f780cac815d383f27167c54
yasaminashoori <yasaminashoori18@gmail.com>
2023-09-29 21:27:09 +0330

    switch

--- englishCourse/01-Fundamentals-Part-1/starter/script.js
+++ englishCourse/01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,15 @@ if (js === "amazing") alert("JavaScript is fun!");
 48 + 2 + 12;
 console.log(48 + 2 + 12);
 
+const numberChecker = 18;
+
+switch (numberChecker) {
+    case 12:
+        console.log("this is an even number.");
+        break;
+    case 13:
+        console.log("this is an odd number.");
+        break;
+    default:
+        console.log("this is not a number");
+}
