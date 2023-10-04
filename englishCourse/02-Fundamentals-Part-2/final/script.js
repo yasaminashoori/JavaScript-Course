ce371866753b1f87cb6373cfdd2c2992736aadcb
yasaminashoori <yasaminashoori18@gmail.com>
2023-10-04 10:46:01 +0330

    add

--- englishCourse/02-Fundamentals-Part-2/final/script.js
+++ englishCourse/02-Fundamentals-Part-2/final/script.js
@@ -122,8 +122,7 @@ A team ONLY wins if it has at least DOUBLE the average score of the other team. 
 
 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
 2. Use the function to calculate the average for both teams
-3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'),
-and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
+3. Create a function 'checkWinner' that takes the average score of each team as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner to the console, together with the victory points, according to the rule above. Example: "Koalas win (30 vs. 13)".
 4. Use the 'checkWinner' function to determine the winner for both DATA 1 and DATA 2.
 5. Ignore draws this time.
 
@@ -246,8 +245,7 @@ if (friends.includes('Steven')) {
 /*
 Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
 
-1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the
-rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
+1. Write a function 'calcTip' that takes any bill value as an input and returns the corresponding tip, calculated based on the rules above (you can check out the code from first tip calculator challenge if you need to). Use the function type you like the most. Test the function using a bill value of 100.
 2. And now let's use arrays! So create an array 'bills' containing the test data below.
 3. Create an array 'tips' containing the tip value for each bill, calculated from the function you created before.
 4. BONUS: Create an array 'total' containing the total values, so the bill + tip.
